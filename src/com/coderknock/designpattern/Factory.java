0700fe042e2c728ce251b68ff93ad0669956fa54
wshfzhwj <wshfzhwj@126.com>
2016-05-25 13:22:26 +0800

    Initial commit

---
+++ src/com/coderknock/designpattern/Factory.java
@@ -0,0 +1,10 @@
+package com.coderknock.designpattern;
+
+/**
+ * 拿客 www.coderknock.com
+ * 微信公众号 coderknock
+ * 作者：三产
+ * 工厂模式
+ */
+public class Factory {
+}
---
+++ src/com/coderknock/designpattern/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * 拿客 www.coderknock.com
+ * 微信公众号 coderknock
+ * 作者：三产
+ * Java设计模式
+ */
+package com.coderknock.designpattern;
